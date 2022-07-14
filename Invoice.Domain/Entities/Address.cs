489ed5916fd9d744f2176deeae54787d628a3f84
HunterAdv <gustavopaulody@gmail.com>
2022-07-14 20:43:31 -0300

    adicionando validação em Address

--- Invoice.Domain/Entities/Address.cs
+++ Invoice.Domain/Entities/Address.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Invoice.Domain.Entities
 {
@@ -11,5 +12,61 @@ namespace Invoice.Domain.Entities
         public string Country { get; private set; }
         public virtual Client Client { get; private set; }        
         public int ClientId { get; private set; }
+
+        //EF
+        protected Address(){}
+
+        public Address(int number, string district, string city, string state, string country, Client client, int clientId)
+        {
+            Number = number;
+            District = district;
+            City = city;
+            State = state;
+            Country = country;
+            Client = client;
+            ClientId = clientId;
+            _errors = new List<string>();
+        }
+
+        public void ChangeNumber(int number)
+        {
+            Number = number;
+            Validate();
+        }
+
+        public void ChangeDistrict(string district)
+        {
+            District = district;
+            Validate();
+        }
+
+        public void ChangeCity(string city)
+        {
+            City = city;
+            Validate();
+        }
+
+        public void ChangeState(string state)
+        {
+            State = state;
+            Validate();
+        }
+
+        public void ChangeCountry(string country)
+        {
+            Country = country;
+            Validate();
+        }
+
+        public void ChangeClientId(int clientId)
+        {
+            ClientId = clientId;
+            Validate();
+        }
+
+        public override bool Validate()
+        {
+            return true;
+        }
     }
 }
---
+++ Invoice.Domain/Validators/AddressValidator.cs
@@ -0,0 +1,89 @@
+﻿using FluentValidation;
+using Invoice.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice.Domain.Validators
+{
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        public AddressValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("A entidade não pode ser vazia.")
+
+                .NotNull()
+                .WithMessage("A entidade não pode ser nula.");
+
+            RuleFor(x => x.Number)
+                .NotNull()
+                .WithMessage("O nome não pode ser nulo")
+
+                .NotEmpty()
+                .WithMessage("O nome não pode ser vazio");
+
+            RuleFor(x => x.District)
+                .NotNull()
+                .WithMessage("O bairro não pode ser nulo")
+
+                .NotEmpty()
+                .WithMessage("O bairro não pode ser vazio")
+
+                .MinimumLength(3)
+                .WithMessage("O bairro deve ter no mínimo 3 caracteres")
+
+                .MaximumLength(80)
+                .WithMessage("O bairro deve ter no máximo 80 caracteres");
+
+            RuleFor(x => x.City)
+                .NotNull()
+                .WithMessage("A cidade não pode ser nula")
+
+                .NotEmpty()
+                .WithMessage("A cidade não pode ser vazia")
+
+                .MinimumLength(3)
+                .WithMessage("A cidade deve ter no mínimo 3 caracteres")
+
+                .MaximumLength(80)
+                .WithMessage("A cidade deve ter no máximo 80 caracteres");
+
+            RuleFor(x => x.State)
+                .NotNull()
+                .WithMessage("O estado não pode ser nulo")
+
+                .NotEmpty()
+                .WithMessage("O estado não pode ser vazio")
+
+                .MinimumLength(3)
+                .WithMessage("O estado deve ter no mínimo 3 caracteres")
+
+                .MaximumLength(80)
+                .WithMessage("O estado deve ter no máximo 80 caracteres");
+
+            RuleFor(x => x.Country)
+                .NotNull()
+                .WithMessage("O país não pode ser nulo")
+
+                .NotEmpty()
+                .WithMessage("O país não pode ser vazio")
+
+                .MinimumLength(3)
+                .WithMessage("O país deve ter no mínimo 3 caracteres")
+
+                .MaximumLength(80)
+                .WithMessage("O país deve ter no máximo 80 caracteres");
+
+            RuleFor(x => x.ClientId)
+                .NotNull()
+                .WithMessage("O bairro não pode ser nulo")
+
+                .NotEmpty()
+                .WithMessage("O bairro não pode ser vazio");
+        }
+    }
+}
