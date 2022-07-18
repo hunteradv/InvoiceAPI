d6fc7eb408ac8d3e5615b95e2a94f644d1cab2e7
HunterAdv <gustavopaulody@gmail.com>
2022-07-18 14:43:50 -0300

    validando entidade Item

--- Invoice.Domain/Entities/Item.cs
+++ Invoice.Domain/Entities/Item.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using InvoiceApi.Domain.Validators;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace InvoiceApi.Domain.Entities
 {
     public class Item : Base
     {
+
         public string Description { get; private set; }        
         public decimal UnitValue { get; private set; }        
         public int Quantity { get; private set; }        
@@ -12,5 +15,39 @@ namespace InvoiceApi.Domain.Entities
         public int InvoiceId { get; private set; }
         public virtual Product Product { get; private set; }        
         public int ProductId { get; private set; }
+
+        //EF
+        public Item()
+        {
+
+        }
+
+        public Item(string description, decimal unitValue, int quantity, decimal totalItem, int invoiceId, int productId)
+        {
+            Description = description;
+            UnitValue = unitValue;
+            Quantity = quantity;
+            TotalItem = totalItem;
+            InvoiceId = invoiceId;
+            ProductId = productId;
+        }
+
+        public override bool Validate()
+        {
+            var validator = new ItemValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                {
+                    _errors.Add(error.ErrorMessage);
+
+                    throw new Exception("Alguns campos estão inválidos, por favor corrija-os!" + _errors[0]);
+                }
+            }
+
+            return true;
+        }
     }
 }
---
+++ Invoice.Domain/Validators/ItemValidator.cs
@@ -0,0 +1,71 @@
+﻿using FluentValidation;
+using InvoiceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApi.Domain.Validators
+{
+    public class ItemValidator : AbstractValidator<Item>
+    {
+        public ItemValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("A entidade não pode ser vazia")
+                
+                .NotNull()
+                .WithMessage("A entidade não pode ser nula");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("A descrição não pode ser vazia")
+
+                .NotNull()
+                .WithMessage("A descrição não pode ser nula")
+                
+                .MinimumLength(3)
+                .WithMessage("A descriçao deve ter no mínimo 3 caracteres")
+                
+                .MaximumLength(80)
+                .WithMessage("A descrição deve ter no máximo 80 caracteres");
+
+            RuleFor(x => x.UnitValue)
+                .NotEmpty()
+                .WithMessage("O valor unitário não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O valor uniário não pode ser nulo");
+
+            RuleFor(x => x.Quantity)
+                .NotEmpty()
+                .WithMessage("A quantidade não pode ser vazia")
+
+                .NotNull()
+                .WithMessage("A quantidade não pode ser nula");
+
+            RuleFor(x => x.TotalItem)
+                .NotEmpty()
+                .WithMessage("O total do item não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O total do item não pode ser nulo");
+
+            RuleFor(x => x.InvoiceId)
+                .NotEmpty()
+                .WithMessage("O id da nota não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O id da nota não pode ser nulo");
+
+            RuleFor(x => x.ProductId)
+                .NotEmpty()
+                .WithMessage("O id do produto não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O id do produto não pode ser nulo");
+        }
+    }
+}
