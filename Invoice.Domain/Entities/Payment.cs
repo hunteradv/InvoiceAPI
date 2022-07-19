88223e085c45197e40d22b34008316e49f0e4fa3
HunterAdv <gustavopaulody@gmail.com>
2022-07-19 12:22:36 -0300

    inserindo validação na entidade Payment

--- Invoice.Domain/Entities/Payment.cs
+++ Invoice.Domain/Entities/Payment.cs
@@ -1,13 +1,47 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using InvoiceApi.Domain.Validators;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace InvoiceApi.Domain.Entities
 {
     public class Payment : Base
-    {               
+    {
+
         public string Description { get; private set; }        
         public decimal Value { get; private set; }        
         public string PaymentType { get; private set; }
         public virtual Invoice Invoice { get; private set; }        
         public int InvoiceId { get; private set; }
+
+        public Payment()
+        {
+
+        }
+
+        public Payment(string description, decimal value, string paymentType, int invoiceId)
+        {
+            Description = description;
+            Value = value;
+            PaymentType = paymentType;
+            InvoiceId = invoiceId;
+        }
+
+        public override bool Validate()
+        {
+            var validator = new PaymentValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                {
+                    _errors.Add(error.ErrorMessage);
+
+                    throw new Exception("Alguns campos estão inválidos, por favor corrija-os!" + _errors[0]);
+                }
+            }
+
+            return true;
+        }
     }
 }
---
+++ Invoice.Domain/Validators/PaymentValidator.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using InvoiceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApi.Domain.Validators
+{
+    public class PaymentValidator : AbstractValidator<Payment>
+    {
+        public PaymentValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("A entidade não pode ser vazia")
+                
+                .NotNull()
+                .WithMessage("A entidade não pode ser nula");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("A descrição não pode ser vazia")
+
+                .NotNull()
+                .WithMessage("A descrição não pode ser nula")
+                
+                .MinimumLength(3)
+                .WithMessage("A descrição deve conter no mínimo 3 caracteres")
+
+                .MinimumLength(80)
+                .WithMessage("A descrição deve conter no máximo 80 caracteres");
+
+            RuleFor(x => x.Value)
+                .NotEmpty()
+                .WithMessage("O valor não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O valor não pode ser nulo");
+
+            RuleFor(x => x.PaymentType)
+                .NotEmpty()
+                .WithMessage("O tipo do pagamento não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O valor não pode ser nulo");
+
+            RuleFor(x => x.InvoiceId)
+                .NotEmpty()
+                .WithMessage("O id da nota não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O id da nota não pode ser nulo");
+        }
+    }
+}
