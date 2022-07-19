f3836d0689be9b1b623784b1b5bb82b085ab003e
HunterAdv <gustavopaulody@gmail.com>
2022-07-19 20:15:28 -0300

    incluindo IProductRepository

---
+++ Invoice.Infrastructure/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using InvoiceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApi.Infrastructure.Interfaces
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+        Task<Product> SearchByName(string name);
+        Task<Product> SearchByUnitValue(decimal unitValue);
+    }
+}
