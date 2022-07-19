7f2f42c683338b92b5d317a00c96f7be26ba7a78
HunterAdv <gustavopaulody@gmail.com>
2022-07-19 14:05:47 -0300

    inserindo validação na entidade product

--- Invoice.Domain/Entities/Product.cs
+++ Invoice.Domain/Entities/Product.cs
@@ -1,12 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using InvoiceApi.Domain.Validators;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace InvoiceApi.Domain.Entities
 {
     public class Product : Base
-    {                
+    {
+
         public string Name { get; private set; }       
         public decimal UnitValue { get; private set; }
         public virtual List<Item> Items { get; private set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, decimal unitValue)
+        {
+            Name = name;
+            UnitValue = unitValue;
+        }
+
+        public override bool Validate()
+        {
+            var validator = new ProductValidate();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                {
+                    _errors.Add(error.ErrorMessage);
+
+                    throw new Exception("Alguns campos estão inválidos, por favor corrija-os!" + _errors[0]);
+                }
+            }
+
+            return true;
+        }
     }
 }
---
+++ Invoice.Domain/Validators/ProductValidate.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using InvoiceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApi.Domain.Validators
+{
+    public class ProductValidate : AbstractValidator<Product>
+    {
+        public ProductValidate()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("A entidade não pode ser vazia")
+                
+                .NotNull()
+                .WithMessage("A entidade não pode ser nula");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("O nome não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O nome não pode ser nulo")
+                
+                .MinimumLength(3)
+                .WithMessage("O nome pode conter no mínimo 3 caracteres")
+                
+                .MaximumLength(80)
+                .WithMessage("O nome pode conter no máximo 80 caracteres");
+
+            RuleFor(x => x.UnitValue)
+                .NotEmpty()
+                .WithMessage("O valor unitário não pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O valor unitário não pode ser nulo");
+        }
+    }
+}
