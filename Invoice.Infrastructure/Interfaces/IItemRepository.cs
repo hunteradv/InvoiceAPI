a67849dca3c36c88371d2c6dbf02de6767599d43
HunterAdv <gustavopaulody@gmail.com>
2022-07-19 20:13:56 -0300

    incluindo Item e Payment Repository

---
+++ Invoice.Infrastructure/Interfaces/IItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using InvoiceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApi.Infrastructure.Interfaces
+{
+    public interface IItemRepository : IBaseRepository<Item>
+    {
+        Task<Item> SearchByTotalItem(decimal totalItem);
+        Task<Item> SearchByDescription(string description);
+    }
+}
---
+++ Invoice.Infrastructure/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,16 @@
+﻿using InvoiceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApi.Infrastructure.Interfaces
+{
+    public interface IPaymentRepository : IBaseRepository<Payment>
+    {
+        Task<Payment> SearchByDescription(string description);
+        Task<Payment> SearchByValue(decimal value);
+        Task<Payment> SearchByPaymentType(string paymentType);
+    }
+}
