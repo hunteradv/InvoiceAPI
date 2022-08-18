776fde42b3e18f3351057c8876e22ba275674101
HunterAdv <gustavopaulody@gmail.com>
2022-08-18 14:16:43 -0300

    - Criando ClientController e método Create para fazer o primeiro teste da Api

---
+++ InvoiceApi.Api/Controllers/ClientController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using InvoiceApi.Api.Utilities;
+using InvoiceApi.Api.ViewModels;
+using InvoiceApi.Core.Exceptions;
+using InvoiceApi.Services.DTO;
+using InvoiceApi.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace InvoiceApi.Api.Controllers
+{
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+        private readonly IMapper _mapper;
+
+        public ClientController(IClientService clientService, IMapper mapper)
+        {
+            _clientService = clientService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("/api/v1/client/create")]
+        public async Task<IActionResult> Create([FromBody] CreateClientViewModel clientViewModel)
+        {
+            try
+            {
+                var clientDTO = _mapper.Map<ClientDTO>(clientViewModel);
+                var contactCreated = await _clientService.Create(clientDTO);
+
+                return Ok(new ResultViewModel
+                {
+                    Message = "Cliente criado com sucesso!",
+                    Success = true,
+                    Data = null
+                });
+            }
+            catch (DomainException e)
+            {
+                return BadRequest(Responses.DomainErrorMessage(e.Message, e.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplictationErrorMessage());
+            }
+        }
+    }
+}
